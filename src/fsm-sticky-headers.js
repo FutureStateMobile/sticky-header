806a16020d34c071bd524c81f1693833e79609e1
Keith Larsen <keithnlarsen@gmail.com>
2014-09-23 09:46:01 -0600

    Update fsm-sticky-headers.js
    Refactored the code, added resizing of the the header on resize of the window

--- src/fsm-sticky-headers.js
+++ src/fsm-sticky-headers.js
@@ -8,37 +8,48 @@ angular.module('fsm', [])
 			contentSelector: '='
 		},
 		link: function(scope, element, attributes, control){
-			var clonedHeader;
-			var content;
+			var header = $(element);
+			var stickyBody = $(scope.contentSelector);
+			var clonedHeader = header.clone();
 
-			var content = $(scope.contentSelector);
-
-			clonedHeader = $(element, this);
-			clonedHeader
-		    	.before(clonedHeader.clone())
-		    	.css("width", clonedHeader.width())
-		    	.css({
+			function initialize(){
+				clonedHeader.css({
 		    		top: scope.topOfPage,
-		    		width: clonedHeader.width(),
-		    		left: clonedHeader.offset().left,
 		    		position: 'fixed',
-      				'z-index': 10001,
-      				visibility: 'hidden',
-      				'box-shadow': '0 2px 4px 0 rgba(0,0,0,.15)'
+	  				'z-index': 10001,
+	  				visibility: 'hidden'
 		    	});
+		    	clonedHeader.addClass( 'sticky-header' );
 
-			function updateStickyHeader(){
-				var offset = content.offset();
+				calculateSize();
+
+				// Attach the new header beside the original one in the dom
+				header.after(clonedHeader);
+			};
+
+			function determineVisibility(){
+				var offset = stickyBody.offset();
 				var scrollTop = $(window).scrollTop() + scope.topOfPage;
 
-				if ((scrollTop > offset.top) && (scrollTop < offset.top + content.height())) {
+				if ((scrollTop > offset.top) && (scrollTop < offset.top + stickyBody.height())) {
 					clonedHeader.css({ "visibility": "visible"});
 				} else {
 					clonedHeader.css( {"visibility": "hidden"} );      
 				};
 			};
 
-			$( window ).scroll( updateStickyHeader ).trigger( "scroll" );
+			function calculateSize(){
+				clonedHeader.css({
+		    		width: header.width(),
+		    		left: header.offset().left
+				});
+			};
+
+			$( window ).scroll( determineVisibility ).trigger( "scroll" );
+
+			$( window ).resize(calculateSize);
+
+			initialize();
 		}
-	}
+	};
 });
